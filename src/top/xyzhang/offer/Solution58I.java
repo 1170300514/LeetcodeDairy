241be355abe1904268d1305326185bb183f74dba
Haoyu <xyzhang.top@foxmail.com>
2021-03-18 15:14:11 +0800

    剑指offer58I第二种方法

--- src/top/xyzhang/offer/Solution58I.java
+++ src/top/xyzhang/offer/Solution58I.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Solution58I {
     public static void main(String[] args) {
         Solution58I solution58I = new Solution58I();
-        System.out.println(solution58I.reverseWords("a good   example"));
+        System.out.println(solution58I.reverseWords3("a good   example"));
     }
     // 1. 转为字符数组遍历
     public String reverseWords(String s) {
@@ -31,11 +31,28 @@ public class Solution58I {
         }
         return ans;
     }
-    // 2. 正则表达式
+    // 2. 分割倒叙法 也可以使用正则表达式但是效率较低
     public String reverseWords2(String s) {
-        String ans = "";
-
-
-        return ans;
+        StringBuffer stringBuffer = new StringBuffer();
+        String[] s1 = s.trim().split(" ");
+        for (int i = s1.length-1; i >= 0; i--) {
+            if (!s1[i].equals(" ")) {
+                stringBuffer.append(s1[i] + " ");
+            }
+        }
+        return stringBuffer.toString().trim();
+    }
+    // 3. 双指针
+    public String reverseWords3(String s) {
+        s = s.trim(); // 删除首尾空格
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while(i >= 0) {
+            while(i >= 0 && s.charAt(i) != ' ') i--; // 搜索首个空格
+            res.append(s.substring(i + 1, j + 1) + " "); // 添加单词
+            while(i >= 0 && s.charAt(i) == ' ') i--; // 跳过单词间空格
+            j = i; // j 指向下个单词的尾字符
+        }
+        return res.toString().trim(); // 转化为字符串并返回
     }
 }
---
+++ src/top/xyzhang/offer/Solution58II.java
@@ -0,0 +1,10 @@
+package top.xyzhang.offer;
+
+public class Solution58II {
+    public static void main(String[] args) {
+
+    }
+    public String reverseLeftWords(String s, int n) {
+
+    }
+}
